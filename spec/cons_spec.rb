71c4f6fcf5b8975799cfdbdd2347f955fe9da6e6
Arlen Christian Mart Cuss <ar@len.me>
2012-10-14 22:31:20 +1100

    Shift some tests.

--- spec/cons_spec.rb
+++ spec/cons_spec.rb
@@ -53,35 +53,6 @@ describe Rouge::Cons do
     end
   end
 
-  describe "the index-access getter" do
-    it "should get single elements" do
-      Rouge::Cons[1, 2, 3][0].should eq 1
-      Rouge::Cons[1, 2, 3][1].should eq 2
-    end
-
-    it "should return nil if an element is not present" do
-      Rouge::Cons[1, 2, 3][5].should eq nil
-    end
-
-    it "should work with negative indices" do
-      Rouge::Cons[1, 2, 3][-1].should eq 3
-      Rouge::Cons[1, 2, 3][-2].should eq 2
-    end
-
-    it "should return Arrays for ranges" do
-      Rouge::Cons[1, 2, 3][0..-1].should eq [1, 2, 3]
-      Rouge::Cons[1, 2, 3][0..-2].should eq [1, 2]
-      Rouge::Cons[1, 2, 3][0...-2].should eq [1]
-      Rouge::Cons[1, 2, 3][2...-1].should eq []
-      Rouge::Cons[1, 2, 3][2..-1].should eq [3]
-    end
-  end
-
-  describe "the each method" do
-    it "should return an enumerator without a block" do
-      Rouge::Cons[1].each.should be_an_instance_of Enumerator
-    end
-  end
 end
 
 # vim: set sw=2 et cc=80:
